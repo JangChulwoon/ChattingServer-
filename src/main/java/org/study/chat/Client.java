34e613571cb643d8d407a7fd7e4f00f01bbd8855
DESKTOP-7JJA5K6\lusiu <luisue@gmail.com>
2017-10-08 16:38:09 +0900

    Simply connect socket
    단순 connect Test 진행

--- src/main/java/org/study/chat/Client.java
+++ src/main/java/org/study/chat/Client.java
@@ -1,7 +1,17 @@
 package org.study.chat;
 
-public class Client {
-    public static void main(String[] args) {
+import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
+import java.net.Socket;
+
+@Slf4j
+public class Client {
+    public static void main(String[] args) throws IOException {
+        String url = "localhost";
+        int port = 9000;
+        Socket socket = new Socket(url,port);
+
+        log.info(""+socket.getPort());
     }
 }
--- src/main/java/org/study/chat/Server.java
+++ src/main/java/org/study/chat/Server.java
@@ -1,7 +1,19 @@
 package org.study.chat;
 
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+@Slf4j
 public class Server {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        ServerSocket server = new ServerSocket(9000);
+        log.info("Ready server");
+        Socket client  = server.accept();
+        log.info("Accept client");
+        log.info(""+client.getPort());
     }
 }
