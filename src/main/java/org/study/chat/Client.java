800e6f76048743d7c91a36862d879c6ee1f94fff
DESKTOP-7JJA5K6\lusiu <luisue@gmail.com>
2017-10-08 17:04:13 +0900

    Make simply echo Server

--- src/main/java/org/study/chat/Client.java
+++ src/main/java/org/study/chat/Client.java
@@ -2,8 +2,9 @@ package org.study.chat;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
+import java.util.Scanner;
 
 @Slf4j
 public class Client {
@@ -11,7 +12,21 @@ public class Client {
         String url = "localhost";
         int port = 9000;
         Socket socket = new Socket(url,port);
+        while(true){
+            InputStreamReader in = new InputStreamReader(socket.getInputStream());
+            OutputStreamWriter out = new OutputStreamWriter(socket.getOutputStream());
+            BufferedReader br = new BufferedReader(in);
+            BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
+            BufferedWriter bw = new BufferedWriter(out);
 
-        log.info(""+socket.getPort());
+            String line = null;
+            String print = null;
+            while((line = keyboard.readLine()) !=null){
+                bw.write(line);
+                bw.newLine();
+                bw.flush();
+                log.info(br.readLine());
+            }
+        }
     }
 }
--- src/main/java/org/study/chat/Server.java
+++ src/main/java/org/study/chat/Server.java
@@ -3,7 +3,7 @@ package org.study.chat;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -12,8 +12,25 @@ public class Server {
     public static void main(String[] args) throws IOException {
         ServerSocket server = new ServerSocket(9000);
         log.info("Ready server");
-        Socket client  = server.accept();
-        log.info("Accept client");
-        log.info(""+client.getPort());
+        while (true) {
+            Socket client = server.accept();
+            log.info("Accept client");
+
+            // make I/O object
+            InputStreamReader in = new InputStreamReader(client.getInputStream());
+            OutputStreamWriter out = new OutputStreamWriter(client.getOutputStream());
+            BufferedReader br = new BufferedReader(in);
+            BufferedWriter bw = new BufferedWriter(out);
+
+            String line =  br.readLine();
+            for(; line != null; line = br.readLine()){
+                // print and write
+                log.info(line);
+                bw.write(line);
+                bw.newLine();
+                bw.flush();
+            }
+        }
+
     }
 }
