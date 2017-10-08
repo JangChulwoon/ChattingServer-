aa929706c7f188da6b971c7071b23e9169d8405f
DESKTOP-7JJA5K6\lusiu <luisue@gmail.com>
2017-10-08 15:59:27 +0900

    Initialize project for Intelli J
    프로젝트 초기화

---
+++ src/main/java/org/study/chat/Client.java
@@ -0,0 +1,7 @@
+package org.study.chat;
+
+public class Client {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/study/chat/Server.java
@@ -0,0 +1,7 @@
+package org.study.chat;
+
+public class Server {
+    public static void main(String[] args) {
+        
+    }
+}
